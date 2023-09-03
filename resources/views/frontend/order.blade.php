95844720ea874c040295edf093de4e8726968662
Rajeshpawar48 <rpawar482003@gmail.com>
2023-09-03 15:10:53 +0530

    form edit

--- resources/views/frontend/order.blade.php
+++ resources/views/frontend/order.blade.php
@@ -22,23 +22,109 @@
 
                                     <div class="row">
                                         <div class="mb-3 col-md-6">
-                                            <label class="form-label">First Name</label>
+                                            <label class="form-label">CUSTOMER NAME</label>
                                             <input type="text" class="form-control" placeholder="1234 Main St">
                                         </div>
                                         <div class="mb-3 col-md-6">
+                                            <label class="form-label">CUSTOMER ADD</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">CUSTOMER GSTIN</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PO NUMBER</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">STYLE REF</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">MEASURMENT TAKER 1</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">MEASURMENT TAKER 2</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PO COPY UPLOAD</label>
+                                            <input type="text" class="form-control" placeholder="1234 Main St">
+                                        </div>
+
+
+                                        <div class="mb-3 col-md-6">
                                             <label class="form-label">Email</label>
-                                            <input type="email" class="form-control" placeholder="Email">
+                                            <input type="email" class="form-control" placeholder="Email" name="email1">
+                                        </div>
+
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone1">
                                         </div>
                                         <div class="mb-3 col-md-6">
-                                            <label class="form-label">Password</label>
-                                            <input type="password" class="form-control" placeholder="Password">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" placeholder="Email" name="email2">
                                         </div>
                                         <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone2">
+                                        </div>
+
+
+
+
+
+
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" placeholder="Email"  name="email3">
+                                        </div>
+
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone3">
+                                        </div>
+
+
+
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" placeholder="Email" name="email4">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone4">
+                                        </div>
+
+
+
+
+
+
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" placeholder="Email" name="email5">
+                                        </div>
+
+
+
+                                        {{-- <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone4">
+                                        </div> --}}
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">PHONE</label>
+                                            <input type="password" class="form-control" placeholder="Password" name="phone5">
+                                        </div>
+                                        {{-- <div class="mb-3 col-md-6">
                                             <label>City</label>
                                             <input type="text" class="form-control">
-                                        </div>
+                                        </div> --}}
                                     </div>
-                                    <div class="row">
+                                    {{-- <div class="row">
                                         <div class="mb-3 col-md-4">
                                             <label class="form-label">State</label>
                                             <select id="inputState" class="default-select form-control wide">
@@ -52,16 +138,16 @@
                                             <label class="form-label">Zip</label>
                                             <input type="text" class="form-control">
                                         </div>
-                                    </div>
-                                    <div class="mb-3">
+                                    </div> --}}
+                                    {{-- <div class="mb-3">
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox">
                                             <label class="form-check-label">
                                                 Check me out
                                             </label>
                                         </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Sign in</button>
+                                    </div> --}}
+                                    {{-- <button type="submit" class="btn btn-primary">Sign in</button> --}}
                                 </form>
                             </div>
                         </div>
